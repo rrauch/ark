619fe12c5fb256c56ab26bc9abf17b9d6eb6fe66
Roland Rauch <roland@rauch.com>
2025-04-25 11:30:21 +0200

    code restructuring

---
+++ src/keys/mod.rs
@@ -0,0 +1,220 @@
+mod typed;
+
+use crate::keys::typed::{
+    Bech32Public, Bech32Secret, TypedDerivationIndex, TypedPublicKey, TypedSecretKey,
+};
+use anyhow::anyhow;
+use autonomi::client::key_derivation::DerivationIndex;
+use autonomi::register::RegisterAddress;
+use autonomi::{Client, PublicKey, SecretKey, XorName};
+use bip39::Mnemonic;
+use sn_bls_ckd::derive_master_sk;
+use sn_curv::elliptic::curves::ECScalar;
+use zeroize::Zeroize;
+
+pub(crate) use crate::keys::typed::{TypedOwnedRegister, TypedRegisterAddress};
+
+const HELM_REGISTER_NAME: &str = "/ark/v0/helm/register";
+const DATA_REGISTER_NAME: &str = "/ark/v0/data/register";
+const WORKER_REGISTER_NAME: &str = "/ark/v0/worker/register";
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct HelmRegisterKind;
+pub type HelmRegister = TypedOwnedRegister<HelmRegisterKind, HelmKeySeed>;
+pub type HelmRegisterAddress = TypedRegisterAddress<HelmRegisterKind, HelmKeySeed>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct HelmKeyKind;
+
+impl Bech32Secret for HelmKeyKind {
+    const HRP: &'static str = "arkhelmsec";
+}
+
+pub type HelmKeySeed = TypedDerivationIndex<HelmKeyKind>;
+pub type HelmKey = TypedSecretKey<HelmKeyKind>;
+
+impl HelmKey {
+    pub fn worker_register(&self) -> WorkerRegister {
+        let owner = TypedSecretKey::new(Client::register_key_from_name(
+            self.as_ref(),
+            WORKER_REGISTER_NAME,
+        ));
+
+        WorkerRegister::new(owner)
+    }
+
+    pub fn worker_key(&self, seed: &WorkerKeySeed) -> WorkerKey {
+        self.derive_child(seed)
+    }
+}
+
+pub type PublicHelmKey = TypedPublicKey<HelmKeyKind>;
+
+impl PublicHelmKey {
+    pub fn worker_register(&self) -> WorkerRegisterAddress {
+        WorkerRegisterAddress::new(register_address_from_name(
+            self.as_ref(),
+            WORKER_REGISTER_NAME,
+        ))
+    }
+
+    pub fn worker_key(&self, seed: &WorkerKeySeed) -> PublicWorkerKey {
+        self.derive_child(seed)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct DataRegisterKind;
+pub type DataRegister = TypedOwnedRegister<DataRegisterKind, DataKeySeed>;
+pub type DataRegisterAddress = TypedRegisterAddress<DataRegisterKind, DataKeySeed>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct DataKeyKind;
+
+impl Bech32Secret for DataKeyKind {
+    const HRP: &'static str = "arkdatasec";
+}
+
+pub type DataKeySeed = TypedDerivationIndex<DataKeyKind>;
+pub type DataKey = TypedSecretKey<DataKeyKind>;
+pub type PublicDataKey = TypedPublicKey<DataKeyKind>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct WorkerRegisterKind;
+
+pub type WorkerRegister = TypedOwnedRegister<WorkerRegisterKind, WorkerKeySeed>;
+
+pub type WorkerRegisterAddress = TypedRegisterAddress<WorkerRegisterKind, WorkerKeySeed>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct WorkerKeyKind;
+
+impl Bech32Secret for WorkerKeyKind {
+    const HRP: &'static str = "arkworkersec";
+}
+
+pub type WorkerKeySeed = TypedDerivationIndex<WorkerKeyKind>;
+pub type WorkerKey = TypedSecretKey<WorkerKeyKind>;
+pub type PublicWorkerKey = TypedPublicKey<WorkerKeyKind>;
+
+impl TryFrom<Mnemonic> for ArkSeed {
+    type Error = anyhow::Error;
+
+    fn try_from(mut value: Mnemonic) -> Result<Self, Self::Error> {
+        let mut seed = value.to_seed_normalized("");
+        value.zeroize();
+        let key_bytes = match eip2333(&seed) {
+            Ok(key_bytes) => key_bytes,
+            Err(err) => {
+                seed.zeroize();
+                return Err(err);
+            }
+        };
+        seed.zeroize();
+        Ok(Self::new(SecretKey::from_bytes(key_bytes)?))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct ArkRoot;
+
+pub type ArkSeed = TypedSecretKey<ArkRoot>;
+
+impl ArkSeed {
+    pub fn random() -> (Self, String) {
+        let mnemonic = Mnemonic::generate(24).expect("24 to be a valid word count");
+        let s = mnemonic.to_string();
+
+        let this = Self::try_from(mnemonic).expect("generated mnemonic to lead to valid ark seed");
+        (this, s)
+    }
+
+    pub fn try_from_mnemonic(mut s: String) -> anyhow::Result<Self> {
+        let mnemonic = match Mnemonic::parse_normalized(s.as_str()) {
+            Ok(mnemonic) => mnemonic,
+            Err(err) => {
+                s.zeroize();
+                return Err(err.into());
+            }
+        };
+        s.zeroize();
+
+        Ok(Self::try_from(mnemonic)?)
+    }
+
+    pub fn address(&self) -> ArkAddress {
+        self.public_key().clone()
+    }
+
+    pub fn helm_register(&self) -> HelmRegister {
+        let owner = TypedSecretKey::new(Client::register_key_from_name(
+            self.as_ref(),
+            HELM_REGISTER_NAME,
+        ));
+
+        HelmRegister::new(owner)
+    }
+
+    pub fn helm_key(&self, seed: &HelmKeySeed) -> HelmKey {
+        self.derive_child(seed)
+    }
+
+    pub fn data_register(&self) -> DataRegister {
+        let owner = TypedSecretKey::new(Client::register_key_from_name(
+            self.as_ref(),
+            DATA_REGISTER_NAME,
+        ));
+
+        DataRegister::new(owner)
+    }
+
+    pub fn data_key(&self, seed: &DataKeySeed) -> DataKey {
+        self.derive_child(seed)
+    }
+}
+
+impl Bech32Public for ArkRoot {
+    const HRP: &'static str = "arkaddr";
+}
+
+pub type ArkAddress = TypedPublicKey<ArkRoot>;
+
+impl ArkAddress {
+    pub fn helm_register(&self) -> HelmRegisterAddress {
+        HelmRegisterAddress::new(register_address_from_name(
+            self.as_ref(),
+            HELM_REGISTER_NAME,
+        ))
+    }
+
+    pub fn helm_key(&self, seed: &HelmKeySeed) -> PublicHelmKey {
+        self.derive_child(seed)
+    }
+
+    pub fn data_register(&self) -> DataRegisterAddress {
+        DataRegisterAddress::new(register_address_from_name(
+            self.as_ref(),
+            DATA_REGISTER_NAME,
+        ))
+    }
+
+    pub fn data_key(&self, seed: &DataKeySeed) -> PublicDataKey {
+        self.derive_child(seed)
+    }
+}
+
+fn eip2333(seed: impl AsRef<[u8]>) -> anyhow::Result<[u8; 32]> {
+    // Derive BLS12-381 master secret key from seed using EIP-2333 standard.
+    // Guarantees a valid, non-zero scalar represented as 32 Big-Endian bytes.
+    let key_bytes: [u8; 32] = derive_master_sk(seed.as_ref())
+        .map_err(|e| anyhow!("derive_master_sk error: {}", e))?
+        .serialize() // Get the 32-byte Big-Endian representation
+        .into(); // Convert GenericArray<u8, 32> to [u8; 32]
+    Ok(key_bytes)
+}
+
+fn register_address_from_name(owner: &PublicKey, name: impl AsRef<str>) -> RegisterAddress {
+    let derivation_index =
+        DerivationIndex::from_bytes(XorName::from_content(name.as_ref().as_bytes()).0);
+    RegisterAddress::new(owner.derive_child(derivation_index.as_bytes().as_slice()))
+}
---
+++ src/keys/typed.rs
@@ -0,0 +1,229 @@
+use anyhow::bail;
+use autonomi::client::key_derivation::DerivationIndex;
+use autonomi::register::RegisterAddress;
+use autonomi::{PublicKey, SecretKey};
+use bech32::{Bech32m, EncodeError, Hrp};
+use std::fmt::{Display, Formatter};
+use std::marker::PhantomData;
+use std::str::FromStr;
+use zeroize::Zeroize;
+
+impl<T> From<[u8; 32]> for TypedDerivationIndex<T> {
+    fn from(value: [u8; 32]) -> Self {
+        Self {
+            inner: DerivationIndex::from_bytes(value),
+            _type: Default::default(),
+        }
+    }
+}
+
+impl<T> Into<[u8; 32]> for TypedDerivationIndex<T> {
+    fn into(self) -> [u8; 32] {
+        self.inner.into_bytes()
+    }
+}
+
+impl<T> TryFrom<&[u8]> for TypedDerivationIndex<T> {
+    type Error = anyhow::Error;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        if value.len() != 32 {
+            bail!("value length [{}] != 32", value.len());
+        }
+        let value: [u8; 32] = value.try_into()?;
+        Ok(Self::from(value))
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct TypedDerivationIndex<T> {
+    inner: DerivationIndex,
+    _type: PhantomData<T>,
+}
+
+impl<T> TypedDerivationIndex<T> {
+    pub fn random() -> Self {
+        let seed: [u8; 32] = rand::random();
+        Self::from(seed)
+    }
+}
+
+#[derive(Zeroize, Debug, Clone, PartialEq, Eq)]
+pub struct TypedSecretKey<T> {
+    inner: SecretKey,
+    #[zeroize(skip)]
+    public_key: TypedPublicKey<T>,
+}
+
+impl<T> TypedSecretKey<T> {
+    pub(super) fn new(inner: SecretKey) -> Self {
+        let public_key = TypedPublicKey::from(inner.public_key());
+        Self { inner, public_key }
+    }
+
+    pub(super) fn public_key(&self) -> &TypedPublicKey<T> {
+        &self.public_key
+    }
+
+    pub(super) fn derive_child<C>(&self, idx: &TypedDerivationIndex<C>) -> TypedSecretKey<C> {
+        TypedSecretKey::new(self.inner.derive_child(idx.inner.as_bytes()))
+    }
+
+    pub(crate) fn as_ref(&self) -> &SecretKey {
+        &self.inner
+    }
+}
+
+pub(super) trait Bech32Secret {
+    const HRP: &'static str;
+}
+
+impl<T: Bech32Secret> TypedSecretKey<T> {
+    pub fn danger_to_string(&self) -> String {
+        let hrp = Hrp::parse(T::HRP).expect("hrp to be valid");
+        bech32::encode::<Bech32m>(hrp, self.inner.to_bytes().as_slice())
+            .expect("bytes to be encodable")
+    }
+}
+
+impl<T: Bech32Secret> FromStr for TypedSecretKey<T> {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let expected_hrp = Hrp::parse(T::HRP).expect("hrp to be valid");
+        let (hrp, mut bytes) = bech32::decode(s.as_ref())?;
+        if hrp != expected_hrp {
+            bytes.zeroize();
+            bail!("hrp [{}] != [{}]", hrp, expected_hrp);
+        };
+        if bytes.len() != 32 {
+            bytes.zeroize();
+            bail!("invalid key len: [{}] != [{}]", bytes.len(), 32);
+        }
+
+        Ok(Self::new(SecretKey::from_bytes(
+            bytes.try_into().expect("byte vec of len 32"),
+        )?))
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct TypedPublicKey<T> {
+    inner: PublicKey,
+    _type: PhantomData<T>,
+}
+
+impl<T> TypedPublicKey<T> {
+    pub(super) fn derive_child<C>(&self, idx: &TypedDerivationIndex<C>) -> TypedPublicKey<C> {
+        TypedPublicKey::from(self.inner.derive_child(idx.inner.as_bytes()))
+    }
+
+    pub(crate) fn as_ref(&self) -> &PublicKey {
+        &self.inner
+    }
+}
+
+impl<T> From<PublicKey> for TypedPublicKey<T> {
+    fn from(value: PublicKey) -> Self {
+        Self {
+            inner: value,
+            _type: Default::default(),
+        }
+    }
+}
+
+impl<T> Into<PublicKey> for TypedPublicKey<T> {
+    fn into(self) -> PublicKey {
+        self.inner
+    }
+}
+
+pub(super) trait Bech32Public {
+    const HRP: &'static str;
+}
+
+impl<T: Bech32Public> Display for TypedPublicKey<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let hrp = Hrp::parse(T::HRP).expect("hrp to be valid");
+        bech32::encode_to_fmt::<Bech32m, _>(f, hrp, &self.inner.to_bytes().as_ref()).map_err(|e| {
+            match e {
+                EncodeError::Fmt(e) => e,
+                _ => {
+                    // not really sure what to do here
+                    panic!("{}", e)
+                }
+            }
+        })
+    }
+}
+
+impl<T: Bech32Public> FromStr for TypedPublicKey<T> {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let expected_hrp = Hrp::parse(T::HRP).expect("hrp to be valid");
+
+        let (hrp, bytes) = bech32::decode(s.as_ref())?;
+        if hrp != expected_hrp {
+            bail!("hrp [{}] != [{}]", hrp, expected_hrp);
+        };
+        if bytes.len() != 48 {
+            bail!("invalid key len: [{}] != [{}]", bytes.len(), 48);
+        }
+        Ok(Self::from(PublicKey::from_bytes(
+            bytes.try_into().expect("byte vec of len 48"),
+        )?))
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct TypedOwnedRegister<T, V> {
+    owner: TypedSecretKey<T>,
+    address: TypedRegisterAddress<T, V>,
+}
+
+impl<T: Clone, V> TypedOwnedRegister<T, V> {
+    pub(super) fn new(owner: TypedSecretKey<T>) -> Self {
+        let address =
+            TypedRegisterAddress::new(RegisterAddress::new(owner.public_key().clone().into()));
+        Self { owner, address }
+    }
+}
+
+impl<T, V> TypedOwnedRegister<T, V> {
+    pub fn owner(&self) -> &TypedSecretKey<T> {
+        &self.owner
+    }
+
+    pub fn address(&self) -> &TypedRegisterAddress<T, V> {
+        &self.address
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct TypedRegisterAddress<T, V> {
+    inner: RegisterAddress,
+    owner: TypedPublicKey<T>,
+    _type: PhantomData<T>,
+    _value_type: PhantomData<V>,
+}
+
+impl<T, V> TypedRegisterAddress<T, V> {
+    pub(super) fn new(inner: RegisterAddress) -> Self {
+        let owner = TypedPublicKey::from(inner.owner());
+        Self {
+            inner,
+            owner,
+            _type: Default::default(),
+            _value_type: Default::default(),
+        }
+    }
+
+    pub fn owner(&self) -> &TypedPublicKey<T> {
+        &self.owner
+    }
+
+    pub(crate) fn as_ref(&self) -> &RegisterAddress {
+        &self.inner
+    }
+}
